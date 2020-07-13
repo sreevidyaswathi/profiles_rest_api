cb2c18820ba6fb6b8e871d9b3f25e0e707d3e578
Sreevidya Swathi <swathisreevidya@gmail.com>
2020-07-13 13:42:25 +0530

    adding user custom profile model, profile manager and the migrations for our database.

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -33,10 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.staticfiles',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
