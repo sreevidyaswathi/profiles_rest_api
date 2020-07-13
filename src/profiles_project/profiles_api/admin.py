bb833f5bd6f41e7f6daa00bf3c8662aa033b0ae8
Sreevidya Swathi <swathisreevidya@gmail.com>
2020-07-13 13:56:10 +0530

    enabled django admin for user profile model

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+#we are registeringg our model to register with django admin
+admin.site.register(models.UserProfile)
